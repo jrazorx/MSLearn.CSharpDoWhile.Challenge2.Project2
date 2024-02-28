8e4b1b27d00407fb5a0426d7248521b79a529e35
jra <julien.gault.50@gmail.com>
2024-02-28 15:48:49 +0100

    Added the prompt loop and display at the end

--- MSLearn.CSharpDoWhile.Challenge2.Project2/Program.cs
+++ MSLearn.CSharpDoWhile.Challenge2.Project2/Program.cs
@@ -1,6 +1,15 @@
 ﻿string[] validRoles = new string [] { "Administrator", "Manager", "User" };
 string enterRoleNamePrompt = "Enter your role name (";
 
+string? roleInput;
+string transformedRoleInput;
+string validRoleSelected = "";
+bool isValidRoleInput = false;
+
+/*
+ * Creates the prompt "Enter your role name"
+ * with the list of available valid roles
+*/ 
 for (int i = 0; i < validRoles.Length; i++)
 {
     enterRoleNamePrompt += validRoles[i];
@@ -12,4 +21,33 @@ for (int i = 0; i < validRoles.Length; i++)
     else
         enterRoleNamePrompt += ")";
 }
-Console.WriteLine(enterRoleNamePrompt);
+
+/*
+ * Prompts the user to enter his role name
+ * Loops until the role is valid
+ * Displays an error if the input is not valid
+*/
+do
+{
+    Console.WriteLine(enterRoleNamePrompt);
+    roleInput = Console.ReadLine();
+
+    if (roleInput == null)
+        continue;
+
+    transformedRoleInput = roleInput.Trim().ToLower();
+    foreach (string role in validRoles)
+    {
+        if (transformedRoleInput == role.Trim().ToLower())
+        {
+            validRoleSelected = role;
+            isValidRoleInput = true;
+        }
+    }
+
+    if (!isValidRoleInput)
+        Console.Write($"The role name that you entered, \"{transformedRoleInput}\" is not valid. ");
+
+} while (!isValidRoleInput);
+
+Console.WriteLine($"Your input value ({validRoleSelected}) has been accepted.");
