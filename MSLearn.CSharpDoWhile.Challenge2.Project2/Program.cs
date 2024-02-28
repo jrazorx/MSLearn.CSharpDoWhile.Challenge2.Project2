2279bfb7e4b96e28997da8fe030c61997df5a735
jra <julien.gault.50@gmail.com>
2024-02-28 15:19:54 +0100

    Dynamic prompt "enter your role name" in case the list of roles grows

--- MSLearn.CSharpDoWhile.Challenge2.Project2/Program.cs
+++ MSLearn.CSharpDoWhile.Challenge2.Project2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] validRoles = new string [] { "Administrator", "Manager", "User" };
+string enterRoleNamePrompt = "Enter your role name (";
+
+for (int i = 0; i < validRoles.Length; i++)
+{
+    enterRoleNamePrompt += validRoles[i];
+
+    if      (i <  validRoles.Length - 2)
+        enterRoleNamePrompt += ", ";
+    else if (i == validRoles.Length - 2)
+        enterRoleNamePrompt += ", or ";
+    else
+        enterRoleNamePrompt += ")";
+}
+Console.WriteLine(enterRoleNamePrompt);
